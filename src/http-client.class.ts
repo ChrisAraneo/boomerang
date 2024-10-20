6d3a131c37323cb72ace80c2023530f95ddab200
Krzysztof Pająk <chris.araneo@gmail.com>
2024-10-20 18:46:25 +0200

    fix(http-client): wrong ok bool expression

--- src/http-client.class.ts
+++ src/http-client.class.ts
@@ -6,7 +6,7 @@ export class HttpClient {
   get(url: string): Observable<Response> {
     return from(request(url)).pipe(
       map((response) => ({
-        ok: response.statusCode % 100 === 2,
+        ok: Math.floor(response.statusCode / 100) === 2,
         status: response.statusCode,
         body: response.body || null,
       })),
