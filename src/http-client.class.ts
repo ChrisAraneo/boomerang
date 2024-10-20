cb0a207b14cde05cf041b579ca2952168996b513
Krzysztof Pająk <chris.araneo@gmail.com>
2024-10-20 18:35:41 +0200

    feat(http-client): #get

--- src/http-client.class.ts
+++ src/http-client.class.ts
@@ -1,1 +1,16 @@
-export class HttpClient {}
+import { Observable, catchError, from, map, of } from 'rxjs';
+import { request } from 'undici';
+import { Response } from './models/response.type';
+
+export class HttpClient {
+  get(url: string): Observable<Response> {
+    return from(request(url)).pipe(
+      map((response) => ({
+        ok: response.statusCode % 100 === 2,
+        status: response.statusCode,
+        body: response.body || null,
+      })),
+      catchError(() => of({ ok: false })),
+    );
+  }
+}
