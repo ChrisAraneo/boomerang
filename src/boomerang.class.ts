d87e60cb71e47b0cda798d8fdfb8baee8a7a8837
Krzysztof Pająk <chris.araneo@gmail.com>
2024-10-22 18:14:50 +0200

    feat(boomerang): implement basic functionality statuses, #run, #destroy

--- src/boomerang.class.ts
+++ src/boomerang.class.ts
@@ -1,1 +1,61 @@
-export class Boomerang {}
+import {
+  BehaviorSubject,
+  debounceTime,
+  filter,
+  map,
+  mergeMap,
+  Observable,
+  Subscription,
+  tap,
+} from 'rxjs';
+
+import { HttpClient } from './http-client.class';
+import { Logger } from './logger.class';
+
+export class Boomerang {
+  private _statuses = new BehaviorSubject<number | null>(null);
+  private subscription?: Subscription;
+
+  constructor(
+    private readonly url: string,
+    private readonly debounceTime: number,
+    private readonly logger: Logger,
+    private readonly httpClient: HttpClient,
+  ) {}
+
+  get statuses(): Observable<number> {
+    return this._statuses.pipe(
+      tap(() => {
+        this.logger.info(`Throwing boomerang at ${this.url}`);
+      }),
+      mergeMap((status) => {
+        return this.httpClient.get(this.url).pipe(
+          map((response) => ({
+            previousStatus: status,
+            currentStatus: response.status || null,
+          })),
+        );
+      }),
+      tap(({ currentStatus }) => {
+        this.logger.info(
+          `Boomerang hit the target at came back with status ${currentStatus}`,
+        );
+      }),
+      debounceTime(this.debounceTime),
+      tap(({ currentStatus }) => {
+        this._statuses.next(currentStatus);
+      }),
+      map(({ currentStatus }) => currentStatus),
+      filter((status) => typeof status === 'number'),
+      // TODO Play sound
+    );
+  }
+
+  run(): void {
+    this.subscription = this.statuses.subscribe();
+  }
+
+  destroy(): void {
+    this.subscription?.unsubscribe();
+  }
+}
